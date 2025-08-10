b6353daa07694b947fc15b0b7a51b8b2a96f0905
zukhai3604 <vqkhaibill2004@gmail.com>
2025-08-10 22:33:57 +0700

    update src

--- React_CRUD_QLSV/src/data.js
+++ React_CRUD_QLSV/src/data.js
@@ -1,7 +1,7 @@
 export default [
-  { id: '1', Masv: "SV001", Hoten: "Nguyễn Văn A", Email: "nguyenvana@example.com", GioiTinh: "Nam", NgaySinh: "2000-05-12" },
-  { id: '2', Masv: "SV002", Hoten: "Trần Thị B", Email: "tranthib@example.com", GioiTinh: "Nữ", NgaySinh: "2001-08-23" },
-  { id: '3', Masv: "SV003", Hoten: "Lê Văn C", Email: "levanc@example.com", GioiTinh: "Nam", NgaySinh: "1999-12-01" },
-  { id: '4', Masv: "SV004", Hoten: "Phạm Thị D", Email: "phamthid@example.com", GioiTinh: "Nữ", NgaySinh: "2000-03-18" },
-  { id: '5', Masv: "SV005", Hoten: "Hoàng Văn E", Email: "hoangvane@example.com", GioiTinh: "Nam", NgaySinh: "2002-07-09" }
+  { id: '1', Masv: "SV001", Hoten: "Vũ Quang Khải", Email: "vqkhai2004@gmail.com", GioiTinh: "Nam", NgaySinh: "2004-06-03" },
+  { id: '2', Masv: "SV002", Hoten: "Vũ Quang Tèo", Email: "vqt2001@gmail.com", GioiTinh: "Nam", NgaySinh: "2001-05-15" },
+  { id: '3', Masv: "SV003", Hoten: "Vũ Quang Hướng", Email: "vqh2003@gmail.com", GioiTinh: "Nam", NgaySinh: "2003-05-15" },
+  { id: '4', Masv: "SV004", Hoten: "Vũ Quang Quác", Email: "vqq1999@gmail.com", GioiTinh: "Nam", NgaySinh: "1999-01-15" },
+  { id: '5', Masv: "SV005", Hoten: "Vũ Quang Tới", Email: "vqt2000@gmail.com", GioiTinh: "Nam", NgaySinh: "2000-05-15" }
 ];
